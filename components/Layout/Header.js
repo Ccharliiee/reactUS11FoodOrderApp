5336a2f00e7d975d183edb64bfcf8acbc8fb4377
Ccharliiee <27688952+Ccharliiee@users.noreply.github.com>
2022-05-01 22:24:54 -0400

    Add a cart component in Header

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -2,13 +2,14 @@ import React from "react";
 import mealsimg from "../../assets/meals.jpg";
 import mealsimg2 from "../../assets/meals2.jpg";
 import styles from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
   return (
     <>
       <header className={styles.header}>
         <h1>first meal</h1>
-        <button>cart</button>
+        <HeaderCartButton />
       </header>
       <div className={styles["main-image"]}>
         <img src={mealsimg} alt="table of meal" />
