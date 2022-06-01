5e0bfae91139f732d7627ef5d0b61e4517550a5d
Ccharliiee <27688952+Ccharliiee@users.noreply.github.com>
2022-05-31 21:23:02 -0400

    Create a cartData reducer

--- store/CartContextProvider.js
+++ store/CartContextProvider.js
@@ -1,11 +1,36 @@
 import CartContext from "./CartContext";
+import { useReducer } from "react";
+
+const CartInitData = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartDataReducer = (state, action) => {
+  if (action.type === "ADD_ITEM") {
+    const NewItems = state.items.concat(action.item);
+    const NewtotalAmount =
+      state.totalAmount + action.item.amount * action.item.price;
+    return {
+      items: NewItems,
+      totalAmount: NewtotalAmount,
+    };
+  }
+  return CartInitData;
+};
 
 const CartContextProvider = (props) => {
-  const addItem2CartHandler = (item) => {};
+  const [cartState, dispatchCartAction] = useReducer(
+    cartDataReducer,
+    CartInitData
+  );
+  const addItem2CartHandler = (item) => {
+    dispatchCartAction({ type: "ADD_ITEM", item: item });
+  };
   const removeItem2CartHandler = (id) => {};
   const CartContextData = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItem2CartHandler,
     removeItem: removeItem2CartHandler,
   };
