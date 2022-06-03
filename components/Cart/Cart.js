8d69704bfc3569c7a8832d6072bb85cb8b1e6b23
Ccharliiee <27688952+Ccharliiee@users.noreply.github.com>
2022-06-02 22:06:24 -0400

    Replace dummy cart data with context data

--- components/Cart/Cart.js
+++ components/Cart/Cart.js
@@ -1,28 +1,41 @@
+import React, { useContext } from "react";
 import styles from "./Cart.module.css";
 import Modal from "../UI/Modal";
+import CartContext from "../../store/CartContext";
+import CartItemComponent from "./CartItemComponent";
 
 const Cart = (props) => {
+  const CartCtx = useContext(CartContext);
+  const totalPrice = `$${CartCtx.totalAmount.toFixed(2)}`;
+  const isCartEmpty = CartCtx.items.length > 0;
+  const CartItemAddHandler = (item) => {};
+  const CartItemRemoveHandler = (id) => {};
   const cartitems = (
     <ul className={styles["cart-items"]}>
-      {[{ id: "m1", name: "Tuna Sushi", amount: 2, price: 22.99 }].map(
-        (item) => (
-          <li>{item.name}</li>
-        )
-      )}
+      {CartCtx.items.map((item) => (
+        <CartItemComponent
+          item={item}
+          onAdd={CartItemAddHandler.bind(null, item)}
+          onRemove={CartItemRemoveHandler.bind(null, item.id)}
+        />
+      ))}
     </ul>
   );
+
   return (
     <Modal onClose={props.onCloseCart}>
       {cartitems}
       <div className={styles["total"]}>
-        <span>total amount</span>
-        <span>45.98</span>
+        <span>Total Price</span>
+        <span>{totalPrice}</span>
       </div>
       <div className={styles["actions"]}>
         <button className={styles["button--alt"]} onClick={props.onCloseCart}>
           close
         </button>
-        <button className={styles["button"]}>place order</button>
+        {isCartEmpty && (
+          <button className={styles["button"]}>place order</button>
+        )}
       </div>
     </Modal>
   );
