c6dd391b04f0218cf5f83e8c02afe98ca989da03
Ccharliiee <27688952+Ccharliiee@users.noreply.github.com>
2022-06-08 14:01:50 -0400

    Add animation to add to cart action

--- components/Layout/HeaderCartButton.js
+++ components/Layout/HeaderCartButton.js
@@ -1,16 +1,31 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import styles from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/CartContext";
 
 const HeaderCartButton = (props) => {
   const CartCtx = useContext(CartContext);
-  const numofItemsinCart = CartCtx.items.reduce((curNum, item) => {
+  const { items } = CartCtx;
+  const [btnAnime, setbtnAnime] = useState(false);
+  const numofItemsinCart = items.reduce((curNum, item) => {
     return curNum + item.amount;
   }, 0);
+  const btntyles = `${styles.button} ${btnAnime ? styles.bump : ""}`;
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setbtnAnime(true);
+    const animeTimer = setTimeout(() => {
+      setbtnAnime(false);
+    }, 300);
+    return () => {
+      clearTimeout(animeTimer);
+    };
+  }, [items]);
   return (
     <>
-      <button className={styles.button} onClick={props.onCartButtonClick}>
+      <button className={btntyles} onClick={props.onCartButtonClick}>
         <span className={styles.icon}>
           <CartIcon />
         </span>
